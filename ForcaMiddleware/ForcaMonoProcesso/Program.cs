e2a0efacd65e7b2a1a8bc6e1590a4b8185901cf7
suzana <suzanasaeki2@gmail.com>
2020-06-01 22:09:01 -0300

    :tada:

---
+++ ForcaMiddleware/ForcaMonoProcesso/Program.cs
@@ -0,0 +1,14 @@
+﻿using Logic;
+using System;
+
+namespace ForcaMonoProcesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GameLogic.GetHelloWorld());
+
+        }
+    }
+}
---
+++ ForcaMiddleware/Logic/GameLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class GameLogic
+    {
+
+        public static string GetHelloWorld()
+        {
+            return "Hello World!";
+        }
+
+    }
+}
